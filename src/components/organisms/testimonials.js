e1e81080969d0fe0ae24cace2e4ad1ff60f05721
tmint <salah7server@gmail.com>
2026-05-28 10:21:16 +0700

    fix: testimonials over section

--- src/components/organisms/testimonials.js
+++ src/components/organisms/testimonials.js
@@ -2,12 +2,12 @@ import Spline from "@splinetool/react-spline";
 
 export default function Testimonials() {
     return (
-        <section className="relative w-full h-screen">
+        <section className="relative w-full h-screen overflow-hidden">
             <div className="w-full h-full">
                 <div className="absolute left-8 top-5 z-0">
                     <h2 className="display-lg text-red-500/10">TESTIMONIALS</h2>
                 </div>
-                <div className="relative z-1">
+                <div className="relative z-1 w-full h-full">
                     <Spline
                         scene="https://prod.spline.design/IvlzguBkbqDN-UC9/scene.splinecode" 
                     />
