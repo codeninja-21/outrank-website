fda62012b00de09a6b5117d752d3a57546a1cee7
tmint <salah7server@gmail.com>
2026-06-10 14:54:55 +0700

    feat: add resend pakage and intergrate bun

--- src/services/footer.service.js
+++ src/services/footer.service.js
@@ -3,7 +3,6 @@ import { createClient } from "@/utils/supabase/server";
 export default async function getFooter() {
     const supabase = await createClient();
     const { data, error } = await supabase.from('footer').select('*');
-    console.log(data);
 
     return data;
 }
